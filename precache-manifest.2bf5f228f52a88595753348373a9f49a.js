67b36e86a8854dd7ec00c62742c11be8d1869f25
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-24 10:40:22 +0000

    Deploy LeMarck/logos-of-banks to github.com/LeMarck/logos-of-banks.git:gh-pages

---
+++ precache-manifest.2bf5f228f52a88595753348373a9f49a.js
@@ -0,0 +1,566 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e2a7326714378358dce4a9ce6e77f0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09844f68bf0e1e4ad3b6",
+    "url": "/static/css/main.fa092618.chunk.css"
+  },
+  {
+    "revision": "af7799ce4c78be805b77",
+    "url": "/static/js/2.e914a28d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e914a28d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09844f68bf0e1e4ad3b6",
+    "url": "/static/js/main.cf522c7f.chunk.js"
+  },
+  {
+    "revision": "e5db536ddd55463c4ea6",
+    "url": "/static/js/runtime-main.75c16fb8.js"
+  },
+  {
+    "revision": "43e1b8edac5aafa2c0d51c6151508a49",
+    "url": "/static/media/AKBars.43e1b8ed.svg"
+  },
+  {
+    "revision": "cc9a0a8d09ef0820de172715b6529c2f",
+    "url": "/static/media/AKIBank.cc9a0a8d.svg"
+  },
+  {
+    "revision": "c99e8511ab25a6075b029a2ca5e77545",
+    "url": "/static/media/ATB.c99e8511.svg"
+  },
+  {
+    "revision": "97d4b385829176749bfb5490b5b91b96",
+    "url": "/static/media/AbsolutBank.97d4b385.svg"
+  },
+  {
+    "revision": "721c372ebefc61f9f1d3450307627678",
+    "url": "/static/media/ActivBank.721c372e.svg"
+  },
+  {
+    "revision": "03bf066198f8257f35c43153a73483b6",
+    "url": "/static/media/Akcept.03bf0661.svg"
+  },
+  {
+    "revision": "016c8b6a620dc0bfa2f8df993fbdb30d",
+    "url": "/static/media/Alefbank.016c8b6a.svg"
+  },
+  {
+    "revision": "2ce827ee26dee178934eb92284aa8e1f",
+    "url": "/static/media/Alfabank.2ce827ee.svg"
+  },
+  {
+    "revision": "9ddb365c5477d8205dbc634bca87ef51",
+    "url": "/static/media/Almazergienbank.9ddb365c.svg"
+  },
+  {
+    "revision": "2f2b8fdbd85c0ba82787953361cff92a",
+    "url": "/static/media/AresBank.2f2b8fdb.svg"
+  },
+  {
+    "revision": "2a2ca90cddbafbd82a6a099e8e66c6c3",
+    "url": "/static/media/Avangard.2a2ca90c.svg"
+  },
+  {
+    "revision": "d9cb7f58597bcd00413f3ecfa38f2d48",
+    "url": "/static/media/Avtogradbank.d9cb7f58.svg"
+  },
+  {
+    "revision": "80fddaa24d493a09936ef890676c476b",
+    "url": "/static/media/BKSPremier.80fddaa2.svg"
+  },
+  {
+    "revision": "fad44b65c989c04423bfbda6606a1175",
+    "url": "/static/media/BaikalInvestBank.fad44b65.svg"
+  },
+  {
+    "revision": "cb89fb79fea48480d244567dd1906096",
+    "url": "/static/media/BaltInvestBank.cb89fb79.svg"
+  },
+  {
+    "revision": "48b3cbe5fb2b1271818370af84baa5e0",
+    "url": "/static/media/BankForshtadt.48b3cbe5.svg"
+  },
+  {
+    "revision": "076e9542945f7283230e4483690f18ba",
+    "url": "/static/media/BankRazvitieStolica.076e9542.svg"
+  },
+  {
+    "revision": "aaf13c472a7a059f1ba4ed545f7f2f14",
+    "url": "/static/media/BankRossii.aaf13c47.svg"
+  },
+  {
+    "revision": "642b227582e7a515ff1cf5188cdd037b",
+    "url": "/static/media/BankRossiya.642b2275.svg"
+  },
+  {
+    "revision": "534acf111ced5b256fd4d052387d15ca",
+    "url": "/static/media/BankSnezhinskij.534acf11.svg"
+  },
+  {
+    "revision": "f585f543f96d792880a8f7bbb5fd3b28",
+    "url": "/static/media/Binbank.f585f543.svg"
+  },
+  {
+    "revision": "30b0e14c7b2d6a84aa7524db29d9c48c",
+    "url": "/static/media/Bistrobank.30b0e14c.svg"
+  },
+  {
+    "revision": "cd33df2c6aa4f36edeaba07e61e1c4b0",
+    "url": "/static/media/CentrInvest.cd33df2c.svg"
+  },
+  {
+    "revision": "b469329adbdc29d93364696f30e3a945",
+    "url": "/static/media/CentrocreditBank.b469329a.svg"
+  },
+  {
+    "revision": "ccb09b864ca03de3463ac8fb64803b18",
+    "url": "/static/media/Cetelem.ccb09b86.svg"
+  },
+  {
+    "revision": "5467e3c777b81c934617adb9c36abed8",
+    "url": "/static/media/Chelindbank.5467e3c7.svg"
+  },
+  {
+    "revision": "1feb0d8dc775929beb694528b9230229",
+    "url": "/static/media/ChelyabInvestBank.1feb0d8d.svg"
+  },
+  {
+    "revision": "5b682c6770147248a8ba87af221437a4",
+    "url": "/static/media/Citibank.5b682c67.svg"
+  },
+  {
+    "revision": "3835dcf68672a9a565d638a1c2ee95c2",
+    "url": "/static/media/CreditUralBank.3835dcf6.svg"
+  },
+  {
+    "revision": "fdcc009a121475c0c1147076d9553918",
+    "url": "/static/media/Databank.fdcc009a.svg"
+  },
+  {
+    "revision": "74e0c6e5a381896ca1642e6f0427d668",
+    "url": "/static/media/Delobank.74e0c6e5.svg"
+  },
+  {
+    "revision": "3f195800487c570230eb38a6af13c2d7",
+    "url": "/static/media/DomRF.3f195800.svg"
+  },
+  {
+    "revision": "d1549a087d1f03b9dd730b851ea7987e",
+    "url": "/static/media/EMB.d1549a08.svg"
+  },
+  {
+    "revision": "c8c023a2257a1d1ec31bf1e58dcda87b",
+    "url": "/static/media/EnergoTransBank.c8c023a2.svg"
+  },
+  {
+    "revision": "9884e85bcd8b52512daf14fa6ab6ed06",
+    "url": "/static/media/Expobank.9884e85b.svg"
+  },
+  {
+    "revision": "bf18de0cb84c1910d56d3a2cbcaf6b06",
+    "url": "/static/media/FinservisBank.bf18de0c.svg"
+  },
+  {
+    "revision": "15b59006ae99f12c232bb573494a7f75",
+    "url": "/static/media/GarantInvest.15b59006.svg"
+  },
+  {
+    "revision": "e703e79491b554c41055073118fe09d1",
+    "url": "/static/media/Gazenergobank.e703e794.svg"
+  },
+  {
+    "revision": "3835dcf68672a9a565d638a1c2ee95c2",
+    "url": "/static/media/Gazprombank.3835dcf6.svg"
+  },
+  {
+    "revision": "f16fb443875f77cc1e45ebbb29e5891f",
+    "url": "/static/media/Gaztransbank.f16fb443.svg"
+  },
+  {
+    "revision": "ccdf5e32875450a434782c3a95bcf827",
+    "url": "/static/media/GenBank.ccdf5e32.svg"
+  },
+  {
+    "revision": "c3ed388db876efd64918402686b46df0",
+    "url": "/static/media/HakasskijMunicipalnyjBank.c3ed388d.svg"
+  },
+  {
+    "revision": "70ca9dc155a28160a8f24856bfa69b04",
+    "url": "/static/media/HomeCreditBank.70ca9dc1.svg"
+  },
+  {
+    "revision": "8f902417f2cd89002b01958224c49f3d",
+    "url": "/static/media/INGBank.8f902417.svg"
+  },
+  {
+    "revision": "f1069f5f546c5a7b477a6658d5fa61b6",
+    "url": "/static/media/InterPromBank.f1069f5f.svg"
+  },
+  {
+    "revision": "13542b278098ac21aa5326a638c556fd",
+    "url": "/static/media/InvestTorgBank.13542b27.svg"
+  },
+  {
+    "revision": "0c59bb5465bcd3b65ad65219cc2551a5",
+    "url": "/static/media/InvesticionnyjKooperativnyjBank.0c59bb54.svg"
+  },
+  {
+    "revision": "dc09ffe78492c9c2f23e09d3c58af2e3",
+    "url": "/static/media/IzhKomBank.dc09ffe7.svg"
+  },
+  {
+    "revision": "3bc9405062c8ccc72fe864b7a4fbc77f",
+    "url": "/static/media/KBHlynov.3bc94050.svg"
+  },
+  {
+    "revision": "8718b4ca51f0729f1dd4773a258c9876",
+    "url": "/static/media/KSBank.8718b4ca.svg"
+  },
+  {
+    "revision": "ac5569e94a0348d1c9214672197e3657",
+    "url": "/static/media/KoltsoUrala.ac5569e9.svg"
+  },
+  {
+    "revision": "2edaa184291de5087e3a159acf126072",
+    "url": "/static/media/KoshelevBank.2edaa184.svg"
+  },
+  {
+    "revision": "a16502444d438edb2e7ac76c884a284d",
+    "url": "/static/media/KreditEvropaBank.a1650244.svg"
+  },
+  {
+    "revision": "41d7b200c91f703fa05ead96fbdeef0f",
+    "url": "/static/media/KubanKredit.41d7b200.svg"
+  },
+  {
+    "revision": "be58f96847ffa064ffd449139ced412c",
+    "url": "/static/media/Levoberezhnyj.be58f968.svg"
+  },
+  {
+    "revision": "0fb5d4dbcc8130d12119d4e38db7850f",
+    "url": "/static/media/Locobank.0fb5d4db.svg"
+  },
+  {
+    "revision": "3e9c651b7c59594cb88157ed6f36f45f",
+    "url": "/static/media/MBBank.3e9c651b.svg"
+  },
+  {
+    "revision": "d8fab7e5d0299da1f2182fb641fa5d0c",
+    "url": "/static/media/MDM.d8fab7e5.svg"
+  },
+  {
+    "revision": "6ea809ffa1fb575baa206f7870e9b28b",
+    "url": "/static/media/MFKBank.6ea809ff.svg"
+  },
+  {
+    "revision": "f3616302f2a41db145a9e6d2efcb54b3",
+    "url": "/static/media/MTSBank.f3616302.svg"
+  },
+  {
+    "revision": "826f63e820d1fd752c412896e91ac2af",
+    "url": "/static/media/MetallInvestBank.826f63e8.svg"
+  },
+  {
+    "revision": "cfd2ebcd18cec2e276d35632b36bc453",
+    "url": "/static/media/Modulbank.cfd2ebcd.svg"
+  },
+  {
+    "revision": "12c11f165a62916b80fe6afa28f74190",
+    "url": "/static/media/MorskojBank.12c11f16.svg"
+  },
+  {
+    "revision": "4f4751985b042323bda91a9b21a2bc9c",
+    "url": "/static/media/MoskovskiyIndustrialniyBank.4f475198.svg"
+  },
+  {
+    "revision": "bde1aa72732428520f7f00cf23d80bc4",
+    "url": "/static/media/MoskovskiyKreditniyBank.bde1aa72.svg"
+  },
+  {
+    "revision": "8adc72582694b1504b0e74ace3ef1f23",
+    "url": "/static/media/Mosoblbank.8adc7258.svg"
+  },
+  {
+    "revision": "05773852a87a478feaf81a31f52c0625",
+    "url": "/static/media/NBDBank.05773852.svg"
+  },
+  {
+    "revision": "ac2d48e5158cfaf48dccc09c3bbbb45a",
+    "url": "/static/media/Neiva.ac2d48e5.svg"
+  },
+  {
+    "revision": "08b0573f1c465125e7bbdce3ebfb0d75",
+    "url": "/static/media/NikoBank.08b0573f.svg"
+  },
+  {
+    "revision": "66c9e84c6d51af64dd072fd108a949eb",
+    "url": "/static/media/NokssBank.66c9e84c.svg"
+  },
+  {
+    "revision": "653a4a136ec447429e4ea97230def88f",
+    "url": "/static/media/Novikombank.653a4a13.svg"
+  },
+  {
+    "revision": "4e098813cabe46be393dad9d94a3f7c2",
+    "url": "/static/media/NovyjVek.4e098813.svg"
+  },
+  {
+    "revision": "c67e9d89c97955afd0f277c1932b6e5e",
+    "url": "/static/media/OTPBank.c67e9d89.svg"
+  },
+  {
+    "revision": "98f4c7a6ed617e282863b1761048f911",
+    "url": "/static/media/Okritie.98f4c7a6.svg"
+  },
+  {
+    "revision": "1317cf7e6a8cb1da89195d9c72035072",
+    "url": "/static/media/Orenburg.1317cf7e.svg"
+  },
+  {
+    "revision": "4e634653c4ae95e1a78d61228aaa27b3",
+    "url": "/static/media/Otkritie.4e634653.svg"
+  },
+  {
+    "revision": "5b1759f1923e3d3e17a0ec32c4985911",
+    "url": "/static/media/PSKB.5b1759f1.svg"
+  },
+  {
+    "revision": "fcc4845a031017cf53dcd0f3d4e3d1d7",
+    "url": "/static/media/PervyjInvesticionnyjBank.fcc4845a.svg"
+  },
+  {
+    "revision": "0881ba9e4218d3ccdba7ec2a85eb85f3",
+    "url": "/static/media/PirBank.0881ba9e.svg"
+  },
+  {
+    "revision": "eca597469e61d40174ca2c84c544f055",
+    "url": "/static/media/Platina.eca59746.svg"
+  },
+  {
+    "revision": "1bbc41a30e207972534e132d0ee777ad",
+    "url": "/static/media/Pochtabank.1bbc41a3.svg"
+  },
+  {
+    "revision": "d12ea78d313846d8efcaf9ac6a931556",
+    "url": "/static/media/PrimSocBank.d12ea78d.svg"
+  },
+  {
+    "revision": "fb7af8507b631f14169faf5c9386d5b6",
+    "url": "/static/media/PromSvyazBank.fb7af850.svg"
+  },
+  {
+    "revision": "7bf7cb906ba26541f0105d407983295d",
+    "url": "/static/media/QiwiBank.7bf7cb90.svg"
+  },
+  {
+    "revision": "c2f793f149659f6910fa51a7643b8faf",
+    "url": "/static/media/RNKB.c2f793f1.svg"
+  },
+  {
+    "revision": "ea3c1b0c3df10d59a57208e8ffb88d7d",
+    "url": "/static/media/Raiffeisenbank.ea3c1b0c.svg"
+  },
+  {
+    "revision": "1d2924f0332814d87471b07cc84dff57",
+    "url": "/static/media/RenessansCredit.1d2924f0.svg"
+  },
+  {
+    "revision": "193ab3aceda3a50fb713759fefb13ca1",
+    "url": "/static/media/ResoCredit.193ab3ac.svg"
+  },
+  {
+    "revision": "afce1d5f90d8841ee3feb128dbe9b26e",
+    "url": "/static/media/Rocketbank.afce1d5f.svg"
+  },
+  {
+    "revision": "9df5bac1a20de8f987b4ad6bf578ff66",
+    "url": "/static/media/RosDorBank.9df5bac1.svg"
+  },
+  {
+    "revision": "33aaf44f7d5f09cacecdba498e336655",
+    "url": "/static/media/RosEvroBank.33aaf44f.svg"
+  },
+  {
+    "revision": "a4577ea8126dfc310f3ba679dd612f5b",
+    "url": "/static/media/RosEximBank.a4577ea8.svg"
+  },
+  {
+    "revision": "c4948a8003e0d19101478841c5aa2e9b",
+    "url": "/static/media/Rosbank.c4948a80.svg"
+  },
+  {
+    "revision": "0a1697f023096f2fca08b9ce3faf3ec9",
+    "url": "/static/media/RoscosmosBank.0a1697f0.svg"
+  },
+  {
+    "revision": "8ffed710fe684b30aafb2266c740f203",
+    "url": "/static/media/Rosgosstrah.8ffed710.svg"
+  },
+  {
+    "revision": "0bcfe45793e4733a3db601a62f3e699f",
+    "url": "/static/media/Rosselhozbank.0bcfe457.svg"
+  },
+  {
+    "revision": "895b0f9868f40c1d58808d9fa5fb5b10",
+    "url": "/static/media/RossiyskyCapital.895b0f98.svg"
+  },
+  {
+    "revision": "d275859ca3d2949d3156290a5c50e366",
+    "url": "/static/media/RostFinans.d275859c.svg"
+  },
+  {
+    "revision": "890dac5a119229232b31b4ea586030c0",
+    "url": "/static/media/RusskiyStandart.890dac5a.svg"
+  },
+  {
+    "revision": "ebae1ebba71fb8831fba6db9097abd0d",
+    "url": "/static/media/SDMBank.ebae1ebb.svg"
+  },
+  {
+    "revision": "a72cebac838af430c44c9c1f9347b752",
+    "url": "/static/media/SKBBank.a72cebac.svg"
+  },
+  {
+    "revision": "55f2e92c29bc8bfe66c9e4e46e99b8d9",
+    "url": "/static/media/SKSBank.55f2e92c.svg"
+  },
+  {
+    "revision": "0e0fe86d2f02af0d001b7737e220dceb",
+    "url": "/static/media/SMPBank.0e0fe86d.svg"
+  },
+  {
+    "revision": "539d69a65441f858f6e14647810f4676",
+    "url": "/static/media/SanktPeterburg.539d69a6.svg"
+  },
+  {
+    "revision": "4d87b6c7a42594ef6390641d36aef1e7",
+    "url": "/static/media/Sberbank.4d87b6c7.svg"
+  },
+  {
+    "revision": "247844980571e43790707e3a5b48095f",
+    "url": "/static/media/Solidarnost.24784498.svg"
+  },
+  {
+    "revision": "ab7a4eaa0fddc608ffb9bda0b60ffc33",
+    "url": "/static/media/Sovkombank.ab7a4eaa.svg"
+  },
+  {
+    "revision": "9958fb69bb1b1145099036de76f5f2af",
+    "url": "/static/media/Soyuz.9958fb69.svg"
+  },
+  {
+    "revision": "178830ba717516719cc75ef70afc5159",
+    "url": "/static/media/Surgetneftegazbank.178830ba.svg"
+  },
+  {
+    "revision": "1f155f5a4a693c0d3f4ee81cb628c954",
+    "url": "/static/media/Svyazbank.1f155f5a.svg"
+  },
+  {
+    "revision": "f451eb856a187b975502561dc5e82a92",
+    "url": "/static/media/TPSB.f451eb85.svg"
+  },
+  {
+    "revision": "6a3b62b1367c349135728840d8d858b1",
+    "url": "/static/media/TavricheskiyBank.6a3b62b1.svg"
+  },
+  {
+    "revision": "1b73062663430fcd1a37bdc544aad50c",
+    "url": "/static/media/TinkoffBank.1b730626.svg"
+  },
+  {
+    "revision": "898ab2ba63bd569e64b37bf267ffcb39",
+    "url": "/static/media/Tochka.898ab2ba.svg"
+  },
+  {
+    "revision": "adc409e78a8f13711b39ae66c638064a",
+    "url": "/static/media/TochkaLight.adc409e7.svg"
+  },
+  {
+    "revision": "0431487614de70dc213dfffb6e8fa4d0",
+    "url": "/static/media/TouchBank.04314876.svg"
+  },
+  {
+    "revision": "f90fdcb62410535c20f05087282e339f",
+    "url": "/static/media/TransCapitalBank.f90fdcb6.svg"
+  },
+  {
+    "revision": "aaf2f9ed442a2de7d60a7b7adacc169a",
+    "url": "/static/media/Trust.aaf2f9ed.svg"
+  },
+  {
+    "revision": "122a5b447c9610d3d882c3a83dc40edb",
+    "url": "/static/media/UBRiR.122a5b44.svg"
+  },
+  {
+    "revision": "c5656d7490676e9cab4ee587309caabc",
+    "url": "/static/media/Uglemetbank.c5656d74.svg"
+  },
+  {
+    "revision": "a55cda5e861085eba3fe9b9469f2ba0b",
+    "url": "/static/media/UniCreditBank.a55cda5e.svg"
+  },
+  {
+    "revision": "74aa17b021cbad22bb56a0480aa39c08",
+    "url": "/static/media/UralFD.74aa17b0.svg"
+  },
+  {
+    "revision": "94c5442e271891379a1b6faa51c01650",
+    "url": "/static/media/Uralprombank.94c5442e.svg"
+  },
+  {
+    "revision": "ab3f3e0660ff336046c962efd44ff77e",
+    "url": "/static/media/Uralsib.ab3f3e06.svg"
+  },
+  {
+    "revision": "7cdefa677cae4f4e7dd2dfe2d45f72b8",
+    "url": "/static/media/Uraltransbank.7cdefa67.svg"
+  },
+  {
+    "revision": "16ead39d9f6ae1bee436eb73151960de",
+    "url": "/static/media/VTB.16ead39d.svg"
+  },
+  {
+    "revision": "de857ee26b8e04d462a4358552dc0109",
+    "url": "/static/media/Venets.de857ee2.svg"
+  },
+  {
+    "revision": "41f585a5adcbe7c49b6100ad17d0894c",
+    "url": "/static/media/VestaBank.41f585a5.svg"
+  },
+  {
+    "revision": "974001d8f5db2789dc918399eccdf783",
+    "url": "/static/media/VladBiznesBank.974001d8.svg"
+  },
+  {
+    "revision": "8914470615c627fb81a2cf01731a95ec",
+    "url": "/static/media/VostochnyBank.89144706.svg"
+  },
+  {
+    "revision": "345eb93a190377b26f57f74949907fc4",
+    "url": "/static/media/Vozrozhdenie.345eb93a.svg"
+  },
+  {
+    "revision": "a23a174f93978d1da3deb9d45ea9dd96",
+    "url": "/static/media/VserossiyskiyBankRazvitiyaRegionov.a23a174f.svg"
+  },
+  {
+    "revision": "66757e49a63ba9126e320a3d6be6ff36",
+    "url": "/static/media/VuzBank.66757e49.svg"
+  },
+  {
+    "revision": "e59ebd980d8f7abecd776ce3e6f6cba8",
+    "url": "/static/media/Zapsibkombank.e59ebd98.svg"
+  },
+  {
+    "revision": "b769aaa01e0beff611a7d80fb5a09af0",
+    "url": "/static/media/Zenit.b769aaa0.svg"
+  }
+]);
