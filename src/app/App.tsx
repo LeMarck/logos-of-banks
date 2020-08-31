39c1b9b95b2d99d4a1cfebff7f8955b17b90dc03
Петров Евгений Сергеевич <petrov@tochka.com>
2020-08-31 12:08:44 +0500

    Refactoring

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,56 +1,15 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
-import { getColor } from './color';
-import * as Icons from './icons';
 import './App.css';
-
-const uploadImage = (src: string): Promise<HTMLImageElement> => new Promise((resolve, reject) => {
-    const img = new Image();
-    img.crossOrigin = 'anonymous';
-    img.onload = (): void => resolve(img);
-    img.onerror = reject;
-    img.src = src;
-});
+import { getColors, ICONS } from './color';
 
 export function App(): JSX.Element {
-    const [icons, setIcons] = useState<Array<string>>([]);
     const [colors, setColors] = useState<Array<string>>([]);
 
-    const main = useCallback((quality: number) => {
-        async function _main() {
-            const newIcons: Array<string> = [];
-            const newColors: Array<string> = [];
-
-            const iconsKeys = Object.keys(Icons);
-
-            for (let index = 0; index < iconsKeys.length; index++) {
-                const imgSrc = (Icons as any)[iconsKeys[index]];
-
-                const image = await uploadImage(imgSrc)
-                let [r, g, b] = getColor(image, quality);
-                const isDark = (r * 299 + g * 587 + b * 114) / 1000 < 128;
-                if (!isDark) {
-                    r *= 0.95;
-                    g *= 0.95;
-                    b *= 0.95;
-                }
-
-                newColors.push(`rgba(${r}, ${g}, ${b}, .${isDark ? 4 : 6})`);
-                newIcons.push(imgSrc);
-            }
-
-            setIcons(newIcons);
-            setColors(newColors);
-        }
-
-        setIcons([]);
-        setColors([]);
-        _main();
-    }, []);
-
     useEffect(() => {
-        main(4);
-    }, [main]);
+        setColors([]);
+        getColors(4).then(setColors);
+    }, []);
 
     return (
         <main className="App">
@@ -61,8 +20,10 @@ export function App(): JSX.Element {
             <section className="App-logos">
                 {colors.map((color, i) =>
                     <div key={i} className={'App-round'}>
-                        <div className={'App-bg'} style={{ background: color }}/>
-                        <img src={icons[i]} className={'App-icon'} alt={''}/>
+                        <div className={'App-bg'} style={{ background: color }}>
+                            <svg width="30" height="30" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M9.17 7h-.92C7.548 7 7 7.559 7 8.222v15.556C7 24.44 7.548 25 8.25 25h13.5c.702 0 1.25-.559 1.25-1.222V8.222C23 7.56 22.452 7 21.75 7h-.92A3.001 3.001 0 0118 9h-6a3.001 3.001 0 01-2.83-2zm11.66-2h.92C23.533 5 25 6.431 25 8.222v15.556C25 25.569 23.533 27 21.75 27H8.25C6.467 27 5 25.569 5 23.778V8.222C5 6.431 6.467 5 8.25 5h.92A3.001 3.001 0 0112 3h6c1.306 0 2.418.835 2.83 2zM11 6a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1zm0 7a1 1 0 100 2h8a1 1 0 100-2h-8zm-1 6a1 1 0 011-1h4a1 1 0 110 2h-4a1 1 0 01-1-1z" fill="#fff"/></svg>
+                        </div>
+                        <img src={ICONS[i]} className={'App-icon'} alt={''}/>
                     </div>
                 )}
             </section>
--- src/app/color.ts
+++ src/app/color.ts
@@ -1,27 +1,28 @@
 // @ts-ignore
 import quantize from 'quantize';
+import * as Icons from './icons';
 
 const ALLOWABLE_COLOR_BOT_LINE = 30;
 const ALLOWABLE_COLOR_TOP_LINE = 170;
 const MAX_RGB_DIFF = 15;
 
-export interface RGBColor {
+interface RGBColor {
     r: number,
     g: number,
     b: number,
 }
 
-export interface RGBAColor extends RGBColor {
+interface RGBAColor extends RGBColor {
     a: number
 }
 
-export const rgba2rgb = ({ r, g, b, a: alpha }: RGBAColor): RGBColor => ({
+const rgba2rgb = ({ r, g, b, a: alpha }: RGBAColor): RGBColor => ({
     r: (1 - alpha / 255) * 255 + (alpha / 255) * r,
     g: (1 - alpha / 255) * 255 + (alpha / 255) * g,
     b: (1 - alpha / 255) * 255 + (alpha / 255) * b
 });
 
-export function isAllowedColor({ r, g, b }: RGBColor): boolean {
+function isAllowedColor({ r, g, b }: RGBColor): boolean {
     const firstDiff = Math.abs(r - g) < MAX_RGB_DIFF;
     const secondDiff = Math.abs(g - b) < MAX_RGB_DIFF;
     const thirdDiff = Math.abs(r - b) < MAX_RGB_DIFF;
@@ -29,7 +30,7 @@ export function isAllowedColor({ r, g, b }: RGBColor): boolean {
     return !(firstDiff && secondDiff && thirdDiff && ((r < ALLOWABLE_COLOR_BOT_LINE) || (r > ALLOWABLE_COLOR_TOP_LINE)));
 }
 
-export function createPixelArray(imgData: Uint8ClampedArray): Array<Array<number>> {
+function createPixelArray(imgData: Uint8ClampedArray): Array<Array<number>> {
     const pixels = imgData;
     const pixelCount = imgData.length;
     const pixelArray = [];
@@ -50,7 +51,7 @@ export function createPixelArray(imgData: Uint8ClampedArray): Array<Array<number
     return pixelArray;
 }
 
-export function getColor(sourceImage: HTMLImageElement, quality: number = 4): [number, number, number] {
+function getColor(sourceImage: HTMLImageElement, quality: number = 4): [number, number, number] {
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d')!;
 
@@ -67,13 +68,14 @@ export function getColor(sourceImage: HTMLImageElement, quality: number = 4): [n
 
     return cmap.palette()[0];
 }
+
 interface HSLColor {
     h: number;
     s: number;
     l: number;
 }
 
-export function RGBToHSL({ r, g, b }: RGBColor): HSLColor {
+function RGBToHSL({ r, g, b }: RGBColor): HSLColor {
     r /= 255;
     g /= 255;
     b /= 255;
@@ -95,4 +97,42 @@ export function RGBToHSL({ r, g, b }: RGBColor): HSLColor {
     l = +(l * 100).toFixed(1);
 
     return { h, s, l };
+}
+
+const uploadImage = (src: string): Promise<HTMLImageElement> => new Promise((resolve, reject) => {
+    const img = new Image();
+
+    img.crossOrigin = 'anonymous';
+    img.onload = (): void => resolve(img);
+    img.onerror = reject;
+    img.src = src;
+});
+
+export const ICONS = Object.values(Icons);
+
+export async function getColors(quality: number): Promise<Array<string>> {
+    const colors: Array<string> = [];
+
+    for (let index = 0; index < ICONS.length; index++) {
+        const imgSrc = ICONS[index];
+
+        const image = await uploadImage(imgSrc)
+        let [r, g, b] = getColor(image, quality);
+        const isDark = (r * 299 + g * 587 + b * 114) / 1000 < 128;
+        if (!isDark) {
+            r *= 0.95;
+            g *= 0.95;
+            b *= 0.95;
+        }
+
+        let { h, s, l } = RGBToHSL({ r, g, b });
+
+        if (!isDark) {
+            s += 10;
+        }
+
+        colors.push(`hsla(${h}, ${s}%, ${l}%, .${isDark ? 4 : 6})`);
+    }
+
+    return colors;
 }
